211ddcca1e4e2daebeed2f853148a68d7aaa9d76
Jonathan Wang <j2279wan@uwaterloo.ca>
2025-11-22 15:40:24 -0800

    Add final basic styling, do some changes for header and a little bit of funny stuff for header

--- src/consts.js
+++ src/consts.js
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "ʕ•ᴥ•ʔ Astro Bear Blog";
+export const SITE_TITLE = "Jonathan's Blog";
 export const SITE_DESCRIPTION = "Astro Bear Blog Theme!";
 export const SITE_URL = "http://localhost:4321";
-export const SEARCH_URL = "https://duckduckgo.com"
+export const SEARCH_URL = "https://duckduckgo.com";
